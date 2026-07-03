577e6267ba446cfe560cd61e64317b4dd6f75a82
Victor Palomino <victormpalominoa@gmail.com>
2026-07-02 20:12:59 -0500

    agrego tipo de vite

--- frontend/src/tipos/declaraciones.d.ts
+++ frontend/src/tipos/declaraciones.d.ts
@@ -14,3 +14,11 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
